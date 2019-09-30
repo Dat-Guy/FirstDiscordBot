ca4bbc7ad48d691dc841fe6d8e51b33df0d2e083
Aidan Smith <aidansmith@Aidans-MacBook-Air-2.local>
2019-09-30 08:09:09 -0400

    First Commit (getting the hang of things

---
+++ FirstDiscordBot.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+
+int main(){
+
+    int x;
+
+    std::cout << "Enter an integer 'x'\n";
+
+    std::cin >> x;
+
+    std::cout << "x = " << x << "\n";
+
+    std::cerr << "Test error...\n";
+
+    return 0;
+
+}
